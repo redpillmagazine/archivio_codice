f4e9dd6627e837c8bf7d1aab4c05b5b39160e514
redpillmagazine <74100105+redpillmagazine@users.noreply.github.com>
2021-05-23 01:43:15 +0200

    Update script.js

--- flipbox/variante5/script.js
+++ flipbox/variante5/script.js
@@ -8,3 +8,7 @@ $(document).ready(function(){
 			$(this).removeClass('flip');
 		});
 	});
+
+
+//inclusione libreria jquery
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
